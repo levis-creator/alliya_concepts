6178d02ede3378cb0413f81a5a8df76de7ebb622
levis-creator <levisluiz@zoho.com>
2023-06-10 02:15:46 +0300

    v1.0.4

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,16 @@
+import img from "../assets/Screenshot 2023-06-09 at 17-49-46 Levis Nyingi.png"
+
+const ProjectCard = () => {
+  return (
+    <div className="shadow-md p-3 rounded-xl border md:absolute">
+    <div>
+      <img src={img} alt="project name" />
+      </div>
+    name of the project
+  </div>
+  
+
+  )
+}
+
+export default ProjectCard
