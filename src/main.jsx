c14fc22643ef0eea26fe909dfed7fcef4189d275
levis-creator <levisluiz@zoho.com>
2023-06-22 02:39:30 +0300

    v1.2.5

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 import PageContextProvider from "./context/PageContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <PageContextProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </PageContextProvider>
   </React.StrictMode>
 );
